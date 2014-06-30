a1c5a657112f4a3d535eb12852b523fa55e69310
Nathanael Burt <nathanael.burt@gmail.com>
2014-06-30 14:20:31 -0600

    seeded database with general interview questions
    Signed-off-by: Seth Musulin <sethmusulin@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Question.create([
+                  {question: 'What is your biggest strength?', category: 'general'},
+                  {question: 'What is your biggest weakness?', category: 'general'},
+                  {question: 'Why do you want this job?', category: 'general'},
+                  {question: 'Why are you leaving your present job?', category: 'general'},
+                  {question: 'When were you most satisfied in your job?', category: 'general'},
+                  {question: 'What did you like least about your last job?', category: 'general'},
+                  {question: "What can you do for us that other candidates can't?", category: 'general'},
+                  {question: 'Can you describe a time when your work was criticized? ', category: 'general'},
+                  {question: 'Give me an example of a time that you felt you went above and beyond the call of duty at work.', category: 'general'},
+                  {question: 'What was the last project you headed up, and what was its outcome?', category: 'general'},
+                  {question: 'Have you ever been on a team where someone was not pulling their own weight? How did you handle it?', category: 'general'},
+                ])
