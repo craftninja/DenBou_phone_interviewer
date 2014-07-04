775742055b0abd0c39ad6356de8858b561dea21f
Seth Musulin <sethmusulin@gmail.com>
2014-07-04 13:33:09 -0600

    committing new schema

--- db/schema.rb
+++ db/schema.rb
@@ -50,12 +50,4 @@ ActiveRecord::Schema.define(version: 20140704161313) do
     t.string "phone_number"
   end
 
-  create_table "users_questions", force: true do |t|
-    t.integer "user_id"
-    t.integer "question_id"
-  end
-
-  add_index "users_questions", ["question_id"], name: "index_users_questions_on_question_id", using: :btree
-  add_index "users_questions", ["user_id"], name: "index_users_questions_on_user_id", using: :btree
-
 end
