9a8f2aa64c7499ce9eef38539e46636b1be865c6
Seth Musulin <sethmusulin@gmail.com>
2014-07-12 15:41:46 -0600

    A non-registered user cannot visit '/recordings' endpoint

--- app/controllers/recordings_controller.rb
+++ app/controllers/recordings_controller.rb
@@ -1,7 +1,13 @@
 class RecordingsController < ApplicationController
-
+  before_action :validate_registration
 
   def index
     @recordings = Recording.all
+  end
+
+  private
+
+  def validate_registration
+    render :file => "#{Rails.root}/public/404.html", :status => 404 unless current_user
   end
 end
--- spec/features/recording_comments_spec.rb
+++ spec/features/recording_comments_spec.rb
@@ -1,8 +1,12 @@
 require 'spec_helper'
 
 describe "Recording Comments" do
-  it "lists all recordings" do
-    user = User.create!(phone_number: '1234567890')
+  it "lists all recordings for registered users and renders error for non-registered users" do
+    mock_auth_hash
+    visit '/'
+    click_link 'Login with LinkedIn'
+    user = User.first
+    user.update(phone_number: '9499499499')
     Recording.create!(user_id: user.id, question_id: 2, recording: "http://recording.com", created_at: "2014-07-06 17:00:17")
     Recording.create!(user_id: user.id, question_id: 1, recording: "http://recording.com", created_at: "2014-07-05 16:00:17")
     visit "/recordings"
@@ -11,5 +15,14 @@ describe "Recording Comments" do
       expect(page).to have_content 'What is your biggest weakness?'
       expect(page).to have_content 'July 5, 2014'
       expect(page).to have_content 'What is your biggest strength?'
-    end
+
+    visit "/users/#{user.id}"
+    click_link "Logout"
+
+    visit "/recordings"
+
+    expect(page).to have_content "The page you were looking for doesn't exist."
+  end
+
+
   end
