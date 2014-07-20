4b96eb35b9f0810735602f9678fac06de651b06a
Nathanael Burt <nathanael.burt@gmail.com>
2014-07-20 16:41:40 -0600

    add questions and separate seeds based on environment

---
+++ db/seeds/test.rb
@@ -0,0 +1,13 @@
+Question.create([
+                  {question: 'What is your biggest strength?', category: 'general'},
+                  {question: 'What is your biggest weakness?', category: 'general'},
+                  {question: 'Why do you want this job?', category: 'general'},
+                  {question: 'Why are you leaving your present job?', category: 'general'},
+                  {question: 'When were you most satisfied in your job?', category: 'general'},
+                  {question: 'What did you like least about your last job?', category: 'general'},
+                  {question: "What can you do for us that other candidates can't?", category: 'general'},
+                  {question: 'Can you describe a time when your work was criticized?', category: 'general'},
+                  {question: 'Give me an example of a time that you felt you went above and beyond the call of duty at work.', category: 'general'},
+                  {question: 'What was the last project you headed up, and what was its outcome?', category: 'general'},
+                  {question: 'Have you ever been on a team where someone was not pulling their own weight? How did you handle it?', category: 'general'},
+                ])
